0f24d6e50681a386e5876b59388d64af3b75e224
Jonathan <jyeh0630@gmail.com>
2025-05-01 20:40:18 -0700

    add font size increase and decease button. Struct JS for font size

--- scripts/setting.js
+++ scripts/setting.js
@@ -1,15 +1,41 @@
 const themeToggleBtn = document.getElementById('themeBtn');
+const increaseFontToggleBtn = document.getElementById('increaseFontBtn');
+const deceaseFontToggleBtn = document.getElementById('desceaseFontBtn');
+
 const bodyElement = document.body;
+const root = document.documentElement;
 const navbar = document.getElementById('navbar');
 
-themeToggleBtn.addEventListener('click', () => {
-  bodyElement.classList.toggle('dark-mode');
+let currentFontSize = 16;
 
-  if (bodyElement.classList.contains('dark-mode')) {
-    navbar.classList.remove('bg-body-tertiary');
-    navbar.classList.add('bg-dark', 'navbar-dark');
-  } else {
-    navbar.classList.remove('bg-dark', 'navbar-dark');
-    navbar.classList.add('bg-body-tertiary');
+// dark mode event listener
+themeToggleBtn.addEventListener('click', () => {
+    //toggle body dark-mode
+    bodyElement.classList.toggle('dark-mode');
+
+    if (bodyElement.classList.contains('dark-mode')) {
+        navbar.classList.remove('bg-body-tertiary');
+        navbar.classList.add('bg-dark', 'navbar-dark');
+    } else {
+        navbar.classList.remove('bg-dark', 'navbar-dark');
+        navbar.classList.add('bg-body-tertiary');
   }
+});
+
+function applyFontSize() {
+    root.style.fontSize = `${currentFontSize}%`;
+}
+
+increaseFontToggleBtn.addEventListener('click', () => {
+    if (currentFontSize < 200) {
+        currentFontSize += 10;
+        applyFontSize();
+    }
+});
+
+deceaseFontToggleBtn.addEventListener('click', () => {
+    if (currentFontSize > 50) {
+        currentFontSize -= 10;
+        applyFontSize();
+    }
 });
