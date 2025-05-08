42fb3651c4ce264e8feb630db8f0cacaf1e73d8a
Berenice  Jean-Louis <bearjenny0909@gmail.com>
2025-05-08 07:23:16 -0700

    Add chatbox.js for chat form toggle

---
+++ scripts/chatbox.js
@@ -0,0 +1,5 @@
+function toggleChatForm() {
+    const chatForm = document.getElementById("chatFormPopup");
+    chatForm.style.display = (chatForm.style.display === "none" || chatForm.style.display === "") ? "block" : "none";
+  }
+  
