6692bf8a078bdf69f4708733607bd242c7fb69ee
Nikolas <callmenik2018@outlook.com>
2025-05-23 15:17:21 -0700

    Validated and corrected html.

--- backend/server.js
+++ backend/server.js
@@ -274,22 +274,6 @@ app.get('/users/:userId', async (req, res) => {
   }
 });
 
-//  Route: Get user document by ID
-// app.get('/users/:userId', async (req, res) => {
-//   const { userId } = req.params;
-
-//   try {
-//     const user = await User.findById(userId);
-//     if (!user) {
-//       return res.status(404).json({ message: "User not found." });
-//     }
-//     res.status(200).json(user);
-//   } catch (error) {
-//     res.status(500).json({ message: "Error fetching user." });
-//   }
-// });
-
-
 // Route: Update user's session field
 app.patch('/users/:userId/session', async (req, res) => {
   const { userId } = req.params;
